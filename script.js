ed7d3612872f213ba33b5b5569b79271b58f9179
Donquixote-00 <wyn.budi.suputra@gmail.com>
2026-09-09 14:50:52 +0800

    Activity Tracker v1.0.3

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+let isRunning = false;
+let seconds = 0;
+let timerId = null;
+
 const startBtn = document.querySelector(".start-btn");
 const pauseBtn = document.querySelector(".pause-btn");
 const stopBtn = document.querySelector(".stop-btn");
 const resetBtn = document.querySelector(".reset-btn");
 
 startBtn.addEventListener("click", function () {
-	console.log("Start clicked!");
+	if (isRunning === true) {
+		return;
+	}
+	isRunning = true;
+	timerId = setInterval(function () {
+		seconds = seconds + 1;
+		console.log(seconds);
+	}, 1000);
 });
 
 pauseBtn.addEventListener("click", function () {
-	console.log("Pause clicked!");
+	clearInterval(timerId);
+	isRunning = false;
+	console.log("Paused!");
 });
 
 stopBtn.addEventListener("click", function () {
